ad997249f5d0f5d4432227416cce387a42b1b2b4
Clemente Morales Fernandez <clemente.morales@verizonwireless.com>
2017-06-16 17:24:29 -0400

    Algorithms feature Add missing code for cyclic lists

--- src/main/java/cracking_the_code_interview/cyclic_list/Solution.java
+++ src/main/java/cracking_the_code_interview/cyclic_list/Solution.java
@@ -2,6 +2,20 @@ package cracking_the_code_interview.cyclic_list;
 
 public class Solution {
 	boolean hasCycle(Node head) {
+		Node tortoiseNode = head;
+		Node hareNode = head;
+		
+		if (hareNode == null)
+			return false;
 
+		while (hareNode != null && hareNode.getNext() != null) {
+			tortoiseNode = tortoiseNode.getNext();
+			hareNode = hareNode.getNext().getNext();
+
+			if (tortoiseNode == hareNode)
+				return true;
+		}
+
+		return false;
 	}
 }
