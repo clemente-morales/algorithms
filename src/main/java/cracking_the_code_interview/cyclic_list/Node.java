cf21b4a1ac091c65ddcd5f7c0f4f4dd72cc04cfb
Clemente Morales Fernandez <clemente.morales@verizonwireless.com>
2017-06-16 15:13:14 -0400

    Algorithms feature Add Cyclick list solution for cracking the code interview

---
+++ src/main/java/cracking_the_code_interview/cyclic_list/Node.java
@@ -0,0 +1,22 @@
+package cracking_the_code_interview.cyclic_list;
+
+public class Node {
+	private final int data;
+    private Node next;
+    
+	public Node(int data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public int getData() {
+		return data;
+	}
+}
---
+++ src/main/java/cracking_the_code_interview/cyclic_list/Solution.java
@@ -0,0 +1,7 @@
+package cracking_the_code_interview.cyclic_list;
+
+public class Solution {
+	boolean hasCycle(Node head) {
+
+	}
+}
